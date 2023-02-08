1c63b555632eef3e74c845c01fb94ad5a26a9294
Dziuba Grzegorz <g.dziuba@e100it.pl>
2023-02-08 12:05:16 +0100

    update app.js v2
    Implementacja funkcji usuwania pojedynczej cyfry z currentScreen oraz poprawa działania filtrowania cyfr na przyciskach

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const allButtons = document.querySelectorAll('.btn')
 const currentScreen = document.querySelector('.screen-current')
+const clear = document.querySelector('.btn-red')
+const deleteBtn = document.querySelector('.btn-blue')
 
+//czyszczenie calego currentscreen
+clear.addEventListener('click', () => {
+	currentScreen.textContent = ''
+})
+//usuwanie jednej cyfry w kalkulatorze
+deleteBtn.addEventListener('click', () => {
+	currentScreen.textContent = currentScreen.textContent.slice(0, -1)
+})
+//wyswietlanie cyfer w current screen
 const displayCurrent = function (event) {
 	const buttonText = event.target.textContent
-	if (buttonText >= '1' && buttonText <= '9') {
+	if (buttonText >= '0' && buttonText <= '9') {
 		currentScreen.textContent += buttonText
 	}
 }
-
+//przefiltrowanie nodelist
 allButtons.forEach(function (button) {
 	button.addEventListener('click', displayCurrent)
 })
